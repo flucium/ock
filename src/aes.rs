d9fed259f3fc5ba7d79127213e59a0aa01da5f40
flucium <14249224+flucium@users.noreply.github.com>
2023-05-02 23:04:06 +0900

    clean ./src/aes.rs

--- src/aes.rs
+++ src/aes.rs
@@ -2,3 +2,14 @@
 
 // type Aes192Gcm = aes_gcm::AesGcm<aes_gcm::aes::Aes192, aead::consts::U12>;
 
+// pub fn aes_128_ctr(){}
+
+// pub fn aes_192_ctr(){}
+
+// pub fn aes_256_ctr(){}
+
+// pub fn aes_128_gcm(){}
+
+// pub fn aes_192_gcm(){}
+
+// pub fn aes_256_gcm(){}

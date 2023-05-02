e7a8c813d0c8c7e7dc1ac73f3792256e8ee29691
flucium <14249224+flucium@users.noreply.github.com>
2023-05-02 13:42:13 +0900

    clean ./src/hash.rs

--- src/hash.rs
+++ src/hash.rs
@@ -1,121 +1,109 @@
-// pub use digest::Digest;
-// use sha2::{Sha224, Sha256,Sha384, Sha512};
+pub use digest::Digest;
+use sha2::{Sha224, Sha256, Sha384, Sha512};
 
-//
-//
-//
-// const BLAKE3_DEFAULT_CONTEXT: &str = "";
+const BLAKE3_DEFAULT_CONTEXT: &str = "";
 
-// pub enum Mac {
-//     Blake3(blake3::Hasher),
-// }
+pub enum Mac {
+    Blake3(blake3::Hasher),
+}
 
-// impl Mac {
-//     pub fn new_blake3(key: [u8; 32]) -> Self {
-//         Self::Blake3(blake3::Hasher::new_keyed(&key))
-//     }
+impl Mac {
+    pub fn new() -> Self {
+        Self::new_blake3()
+    }
 
-//     pub fn update(&mut self, bytes: &[u8]) {
-//         match self {
-//             Self::Blake3(f) => {
-//                 f.update(bytes);
-//             }
-       
-//         }
-//     }
+    pub fn new_blake3() -> Self {
+        Self::Blake3(blake3::Hasher::new())
+    }
 
-//     pub fn finalize(&mut self) -> Vec<u8> {
-//         match self {
-//             Self::Blake3(f) => f.finalize().as_bytes().to_vec(),
-//         }
-//     }
-// }
+    pub fn update(&mut self, bytes: &[u8]) -> &mut blake3::Hasher {
+        match self {
+            Mac::Blake3(f) => f.update(bytes),
+        }
+    }
 
-// pub enum Hash {
-//     Sha224(Sha224),
-//     Sha256(Sha256),
-//     Sha384(Sha384),
-//     Sha512(Sha512),
-//     Blake3(blake3::Hasher),
-// }
+    pub fn finalize(&mut self) -> Vec<u8> {
+        match self {
+            Mac::Blake3(f) => f.finalize().as_bytes().to_vec(),
+        }
+    }
+}
 
-// impl Hash {
-//     pub fn new_blake3() -> Self {
-//         Self::Blake3(blake3::Hasher::new())
-//     }
+impl Default for Mac {
+    fn default() -> Self {
+        Self::Blake3(blake3::Hasher::new())
+    }
+}
 
-//     pub fn new_sha224()->Self{
-//         Self::Sha224(Sha224::new())
-//     }
-    
-//     pub fn new_sha256()->Self{
-//         Self::Sha256(Sha256::new())
-//     }
+pub enum Hash {
+    Blake3(blake3::Hasher),
+    Sha224(Sha224),
+    Sha256(Sha256),
+    Sha384(Sha384),
+    Sha512(Sha512),
+}
 
-//     pub fn new_sha384()->Self{
-//         Self::Sha384(Sha384::new())
-//     }
+impl Hash {
+    pub fn new() -> Self {
+        Self::new_blake3()
+    }
 
-//     pub fn new_sha512()->Self{
-//         Self::Sha512(Sha512::new())
-//     }
+    pub fn new_blake3() -> Self {
+        Self::Blake3(blake3::Hasher::new())
+    }
 
-//     pub fn update(&mut self, bytes: &[u8]) {
-//         match self {
-//             Self::Sha224(f) => {
-//                 f.update(bytes);
-//             }
-//             Self::Sha256(f) => {
-//                 f.update(bytes);
-//             }
-//             Self::Sha384(f)=>{
-//                 f.update(bytes)
-//             }
-//             Self::Sha512(f) => {
-//                 f.update(bytes);
-//             }
-//             Self::Blake3(f) => {
-//                 f.update(bytes);
-//             }
-//         }
-//     }
+    pub fn new_sha224() -> Self {
+        Self::Sha224(Sha224::new())
+    }
 
-//     pub fn finalize(&mut self) -> Vec<u8> {
-//         match self {
-//             Self::Sha224(f) => {
-//                 todo!()
-//             }
-//             Self::Sha256(f) => {
-//                 todo!()
-//             }
-//             Self::Sha384(f)=>{
-//                 todo!()
-//             }
-//             Self::Sha512(f) => {
-//                 todo!()
-//             }
-//             Self::Blake3(f) => f.finalize().as_bytes().to_vec(),
-//         }
-//     }
+    pub fn new_sha256() -> Self {
+        Self::Sha256(Sha256::new())
+    }
 
-//     pub fn finalize_hex(&mut self) -> String {
-//         match self {
-//             Self::Sha224(f) => {
-//                 todo!()
-//             }
-//             Self::Sha256(f) => {
-//                 todo!()
-//             }
-//             Self::Sha384(f)=>{
-//                 todo!()
-//             }
-//             Self::Sha512(f) => {
-//                 todo!()
-//             }
-//             Self::Blake3(f) => f.finalize().to_hex().to_string(),
-//         }
-//     }
-// }
+    pub fn new_sha384() -> Self {
+        Self::Sha384(Sha384::new())
+    }
+
+    pub fn new_sha512() -> Self {
+        Self::Sha512(Sha512::new())
+    }
+
+    pub fn update(&mut self, bytes: &[u8]) {
+        match self {
+            Self::Sha224(f) => f.update(bytes),
+            Self::Sha256(f) => f.update(bytes),
+            Self::Sha384(f) => f.update(bytes),
+            Self::Sha512(f) => f.update(bytes),
+            Self::Blake3(f) => {
+                f.update(bytes);
+            }
+        }
+    }
+
+    pub fn finalize(&mut self) -> Vec<u8> {
+        match self {
+            Self::Sha224(f) => {
+                todo!()
+            }
+            Self::Sha256(f) => {
+                todo!()
+            }
+            Self::Sha384(f) => {
+                todo!()
+            }
+            Self::Sha512(f) => {
+                todo!()
+            }
+            Self::Blake3(f) => f.finalize().as_bytes().to_vec(),
+        }
+    }
+}
+
+impl Default for Hash {
+    fn default() -> Self {
+        Self::Blake3(blake3::Hasher::new())
+    }
+}
 
 //
 // pub enum Kdf {

dbb8859772eb6832f1c1b12e172eff98c20aa576
flucium <14249224+flucium@users.noreply.github.com>
2023-05-11 15:04:01 +0900

    update ./rs/src/utils.rs

--- rs/src/utils.rs
+++ rs/src/utils.rs
@@ -3,3 +3,24 @@
 
 //     x.collect::<String>()
 // }
+
+pub mod format {
+
+    pub struct Bytes(Vec<u8>);
+
+    impl Bytes {}
+
+    impl From<&[u8]> for Bytes {
+        fn from(bytes: &[u8]) -> Self {
+            Self(bytes.to_vec())
+        }
+    }
+
+    impl From<Vec<u8>> for Bytes {
+        fn from(bytes: Vec<u8>) -> Self {
+            Self(bytes)
+        }
+    }
+}
+
+fn demo() {}

bc36fba235e02f57c13595fbdf2aab653e336c8a
flucium <14249224+flucium@users.noreply.github.com>
2023-05-12 11:17:03 +0900

    update ./rs/src/rand.rs

--- rs/src/rand.rs
+++ rs/src/rand.rs
@@ -1,13 +1,43 @@
 use rand::{Rng, SeedableRng};
 use rand_chacha::ChaCha20Rng;
-
-//Rust Crypto
-// pub use aes_gcm::aead::OsRng;
+// pub use rand_chacha::rand_core::OsRng;
 
 pub fn generate() -> [u8; 32] {
     let mut rng = ChaCha20Rng::from_entropy();
 
     let bytes = rng.gen::<[u8; 32]>();
+
+    bytes
+}
+
+pub fn gen_32() -> [u8; 32] {
+    let mut rng = ChaCha20Rng::from_entropy();
+
+    let bytes = rng.gen::<[u8; 32]>();
+
+    bytes
+}
+
+pub fn gen_24() -> [u8; 24] {
+    let mut rng = ChaCha20Rng::from_entropy();
+
+    let bytes = rng.gen::<[u8; 24]>();
+
+    bytes
+}
+
+pub fn gen_16() -> [u8; 16] {
+    let mut rng = ChaCha20Rng::from_entropy();
+
+    let bytes = rng.gen::<[u8; 16]>();
+
+    bytes
+}
+
+pub fn gen_12() -> [u8; 12] {
+    let mut rng = ChaCha20Rng::from_entropy();
+
+    let bytes = rng.gen::<[u8; 12]>();
 
     bytes
 }

da61c6d3c16c756cda5e6dd62b6b92139ec94fe0
flucium <14249224+flucium@users.noreply.github.com>
2023-05-12 14:28:59 +0900

    update ./rs/src/utils.rs

--- rs/src/utils.rs
+++ rs/src/utils.rs
@@ -1,26 +1,3 @@
-// pub fn bytes_to_hex(bytes: &[u8]) -> String {
-//     let x = bytes.iter().map(|b| format!("{:02X}", b));
-
-//     x.collect::<String>()
-// }
-
-pub mod format {
-
-    pub struct Bytes(Vec<u8>);
-
-    impl Bytes {}
-
-    impl From<&[u8]> for Bytes {
-        fn from(bytes: &[u8]) -> Self {
-            Self(bytes.to_vec())
-        }
-    }
-
-    impl From<Vec<u8>> for Bytes {
-        fn from(bytes: Vec<u8>) -> Self {
-            Self(bytes)
-        }
-    }
+pub fn hex(bytes: &[u8]) -> String {
+    bytes.iter().map(|byte| format!("{:02X}", byte)).collect()
 }
-
-fn demo() {}

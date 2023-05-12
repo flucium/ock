adff6aec1b81783949ff3456f60d68c8e035be80
flucium <14249224+flucium@users.noreply.github.com>
2023-05-12 14:41:10 +0900

    update ./rs/src/utils.rs

--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod hash;
 pub mod rand;
 pub mod aes;
+pub mod utils;
--- rs/src/utils.rs
+++ rs/src/utils.rs
@@ -1,3 +1,7 @@
 pub fn hex(bytes: &[u8]) -> String {
     bytes.iter().map(|byte| format!("{:02X}", byte)).collect()
 }
+
+pub fn type_of<T>(_: T) -> String {
+    core::any::type_name::<T>().to_string()
+}

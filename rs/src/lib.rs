5e7e00f6dac80501d5c5446aca9a0ad14ebfd5e5
flucium <14249224+flucium@users.noreply.github.com>
2023-05-12 22:16:00 +0900

    update ./rs/src/lib.rs

--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -1,4 +1,44 @@
+pub mod aes;
 pub mod hash;
 pub mod rand;
-pub mod aes;
 pub mod utils;
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug, Clone, Default)]
+pub enum ErrorKind {
+    #[default]
+    Unknown,
+}
+
+#[derive(Debug, Clone)]
+pub struct Error {
+    kind: ErrorKind,
+    message: String,
+}
+
+impl Error {
+    pub fn new(kind: ErrorKind, message: String) -> Self {
+        Self {
+            kind: kind,
+            message: message,
+        }
+    }
+
+    pub fn message(&self) -> &str {
+        &self.message
+    }
+
+    pub fn kind(&self) -> &ErrorKind {
+        &self.kind
+    }
+}
+
+impl Default for Error {
+    fn default() -> Self {
+        Self {
+            kind: ErrorKind::Unknown,
+            message: Default::default(),
+        }
+    }
+}

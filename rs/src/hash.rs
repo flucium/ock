7af2cbf3e941f283c256fdf3e22d3294daf88eee
flucium <14249224+flucium@users.noreply.github.com>
2023-05-12 14:34:48 +0900

    update ./rs/src/hash.rs

--- rs/src/hash.rs
+++ rs/src/hash.rs
@@ -1,161 +1,42 @@
-pub use digest::Digest;
-// pub use hmac::Mac;
+use digest::Digest;
 
-// use sha2::{Sha224, Sha256, Sha384, Sha512};
-// use sha2::{Sha256, Sha512, Sha512_256};
+pub const BLAKE3_DEFAULT_CONTEXT: &str = "";
 
-// const BLAKE3_DEFAULT_CONTEXT: &str = "";
 
-// pub trait Hasher {
-//     fn update(&mut self, btyes: &[u8]) -> &mut Self;
-//     fn finalize(&mut self) -> Vec<u8>;
-// }
+// BLAKE3
+pub fn blake3(bytes: &[u8]) -> [u8; 32] {
+    blake3::hash(bytes).into()
+}
 
-// // SHA2
-// pub struct Sha256(sha2::Sha256);
+/// BLAKE3 XOF
+pub fn blake3_extend(bytes: &[u8], output: &mut [u8]) {
+    blake3::Hasher::new()
+        .update(bytes)
+        .finalize_xof()
+        .fill(output);
+}
 
-// impl Sha256 {
-//     pub fn new() -> Self {
-//         Self(sha2::Sha256::new())
-//     }
-// }
+/// BLAKE3 Key Derivation
+pub fn blake3_derive_key(ikm: &[u8], context: &str) -> [u8; 32] {
+    blake3::derive_key(context, ikm)
+}
 
-// impl Hasher for Sha256 {
-//     fn update(&mut self, bytes: &[u8]) -> &mut Self {
-//         self.0.update(bytes);
-//         self
-//     }
+/// BLAKE3 MAC
+pub fn blake3_mac(key: &[u8; 32], msg: &[u8]) -> [u8; 32] {
+    blake3::keyed_hash(key, msg).into()
+}
 
-//     fn finalize(&mut self) -> Vec<u8> {
-//         self.0.finalize_reset().to_vec()
-//     }
-// }
+/// SHA2 256
+pub fn sha256(bytes: &[u8]) -> [u8; 32] {
+    sha2::Sha256::digest(bytes).into()
+}
 
-// pub struct Sha512(sha2::Sha512);
+/// SHA2 512
+pub fn sha512(bytes: &[u8]) -> [u8; 64] {
+    sha2::Sha512::digest(bytes).into()
+}
 
-// impl Sha512 {
-//     pub fn new() -> Self {
-//         Self(sha2::Sha512::new())
-//     }
-// }
-
-// impl Hasher for Sha512 {
-//     fn update(&mut self, bytes: &[u8]) -> &mut Self {
-//         self.0.update(bytes);
-//         self
-//     }
-
-//     fn finalize(&mut self) -> Vec<u8> {
-//         self.0.finalize_reset().to_vec()
-//     }
-// }
-
-// pub struct Sha512_256(sha2::Sha512_256);
-
-// impl Sha512_256 {
-//     pub fn new() -> Self {
-//         Self(sha2::Sha512_256::new())
-//     }
-// }
-
-// impl Hasher for Sha512_256 {
-//     fn update(&mut self, bytes: &[u8]) -> &mut Self {
-//         self.0.update(bytes);
-//         self
-//     }
-
-//     fn finalize(&mut self) -> Vec<u8> {
-//         self.0.finalize_reset().to_vec()
-//     }
-// }
-
-// // BLAKE3
-// pub struct Blake3(blake3::Hasher);
-
-// impl Blake3 {
-//     pub fn new() -> Self {
-//         Self(blake3::Hasher::new())
-//     }
-
-//     pub fn new_keyed(key: &[u8; 32]) -> Self {
-//         Self(blake3::Hasher::new_keyed(key))
-//     }
-
-//     pub fn new_derive_key() -> Self {
-//         Self(blake3::Hasher::new_derive_key(BLAKE3_DEFAULT_CONTEXT))
-//     }
-
-//     pub fn new_derive_key_with_context(context: &str) -> Self {
-//         Self(blake3::Hasher::new_derive_key(context))
-//     }
-// }
-
-// impl Hasher for Blake3 {
-//     fn update(&mut self, bytes: &[u8]) -> &mut Self {
-//         self.0.update(bytes);
-//         self
-//     }
-
-//     fn finalize(&mut self) -> Vec<u8> {
-//         self.0.finalize().as_bytes().to_vec()
-//     }
-// }
-
-// // HMAC SHA2
-
-// pub struct HmacSha256(hmac::Hmac<sha2::Sha256>);
-
-// impl HmacSha256 {
-//     pub fn new_keyed(key: &[u8]) -> Self {
-//         Self(hmac::Hmac::<sha2::Sha256>::new_from_slice(key).unwrap())
-//     }
-// }
-
-// impl Hasher for HmacSha256 {
-//     fn update(&mut self, bytes: &[u8]) -> &mut Self {
-//         self.0.update(bytes);
-//         self
-//     }
-
-//     fn finalize(&mut self) -> Vec<u8> {
-//         self.0.to_owned().finalize().into_bytes().to_vec()
-//     }
-// }
-
-// pub struct HmacSha512(hmac::Hmac<sha2::Sha512>);
-
-// impl HmacSha512 {
-//     pub fn new_keyed(key: &[u8]) -> Self {
-//         Self(hmac::Hmac::<sha2::Sha512>::new_from_slice(key).unwrap())
-//     }
-// }
-
-// impl Hasher for HmacSha512 {
-//     fn update(&mut self, bytes: &[u8]) -> &mut Self {
-//         self.0.update(bytes);
-//         self
-//     }
-
-//     fn finalize(&mut self) -> Vec<u8> {
-//         self.0.to_owned().finalize().into_bytes().to_vec()
-//     }
-// }
-
-// pub struct HmacSha512_256(hmac::Hmac<sha2::Sha512_256>);
-
-// impl HmacSha512_256 {
-//     pub fn new_keyed(key: &[u8]) -> Self {
-//         Self(hmac::Hmac::<sha2::Sha512_256>::new_from_slice(key).unwrap())
-//     }
-// }
-
-// impl Hasher for HmacSha512_256 {
-//     fn update(&mut self, bytes: &[u8]) -> &mut Self {
-//         self.0.update(bytes);
-//         self
-//     }
-
-//     fn finalize(&mut self) -> Vec<u8> {
-//         self.0.to_owned().finalize().into_bytes().to_vec()
-//     }
-// }
+/// SHA2 512/256
+pub fn sha512_256(bytes: &[u8]) -> [u8; 32] {
+    sha2::Sha512_256::digest(bytes).into()
+}

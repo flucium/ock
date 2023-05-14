553df88ebdede002453718fe5f80de449bc77337
flucium <14249224+flucium@users.noreply.github.com>
2023-05-14 14:47:40 +0900

    refactor ./rs/src/hash.rs
    blake3_extend: _xof and blake3_derive_key: _kdf

--- rs/src/hash.rs
+++ rs/src/hash.rs
@@ -2,14 +2,13 @@ use digest::Digest;
 
 pub const BLAKE3_DEFAULT_CONTEXT: &str = "";
 
-
 // BLAKE3
 pub fn blake3(bytes: &[u8]) -> [u8; 32] {
     blake3::hash(bytes).into()
 }
 
-/// BLAKE3 XOF
-pub fn blake3_extend(bytes: &[u8], output: &mut [u8]) {
+/// BLAKE3 XOF(extend)
+pub fn blake3_xof(bytes: &[u8], output: &mut [u8]) {
     blake3::Hasher::new()
         .update(bytes)
         .finalize_xof()
@@ -17,7 +16,7 @@ pub fn blake3_extend(bytes: &[u8], output: &mut [u8]) {
 }
 
 /// BLAKE3 Key Derivation
-pub fn blake3_derive_key(ikm: &[u8], context: &str) -> [u8; 32] {
+pub fn blake3_kdf(ikm: &[u8], context: &str) -> [u8; 32] {
     blake3::derive_key(context, ikm)
 }
 
@@ -25,6 +24,8 @@ pub fn blake3_derive_key(ikm: &[u8], context: &str) -> [u8; 32] {
 pub fn blake3_mac(key: &[u8; 32], msg: &[u8]) -> [u8; 32] {
     blake3::keyed_hash(key, msg).into()
 }
+
+// SHA2 Families
 
 /// SHA2 256
 pub fn sha256(bytes: &[u8]) -> [u8; 32] {
@@ -40,3 +41,5 @@ pub fn sha512(bytes: &[u8]) -> [u8; 64] {
 pub fn sha512_256(bytes: &[u8]) -> [u8; 32] {
     sha2::Sha512_256::digest(bytes).into()
 }
+
+// SHA3 Families

1b5d022ff9205e99366fbef5849ee0af8d029842
flucium <14249224+flucium@users.noreply.github.com>
2023-05-17 23:19:29 +0900

    update ./rs/src/utils.rs

--- rs/src/utils.rs
+++ rs/src/utils.rs
@@ -5,3 +5,15 @@ pub fn hex(bytes: &[u8]) -> String {
 pub fn type_of<T>(_: T) -> String {
     core::any::type_name::<T>().to_string()
 }
+
+/// To avoid making silly mistakes.
+/// byte * 8 to get bit.
+pub fn byte_to_bit(n: usize) -> usize {
+    n * 8
+}
+
+/// To avoid making silly mistakes.
+/// bit / 8 to get byte.
+pub fn bit_to_bytes(n: usize) -> usize {
+    n / 8
+}

5ae33cb34a0d39bea8d602554c4267bc038571df
flucium <14249224+flucium@users.noreply.github.com>
2023-05-18 11:52:42 +0900

    update ./rs/src/main.rs

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,33 +1,19 @@
-use aead::{Aead, AeadCore, KeyInit};
-
 fn main() {
-    let payload = aead::Payload { msg: b"", aad: b"" };
-    let key = aes_gcm::Aes256Gcm::generate_key(&mut aead::OsRng);
-    let aes = aes_gcm::Aes256Gcm::new(&key);
-    let nonce = &aes_gcm::Aes256Gcm::generate_nonce(&mut aead::OsRng);
-    for i in 0..10 {
-        let c = aes
-            .encrypt(
-                &nonce,
-                aead::Payload {
-                    msg: format!("hello - {}", i).as_bytes(),
-                    aad: i.to_string().as_bytes(),
-                },
-            )
-            .unwrap();
-        println!(
-            "{}",
-            String::from_utf8_lossy(
-                &aes.decrypt(
-                    nonce,
-                    aead::Payload {
-                        msg: &c,
-                        aad: i.to_string().as_bytes(),
-                    }
-                )
-                .unwrap()
-            )
-            .to_string()
-        );
-    }
+    // let bs = Bytes { data: [9u8; 100] };
+    // print!("{:?}",bs);
+
+    Bytes{data:[0u8;100]};
+    Bytes{data:[0u8;200]};
 }
+
+#[derive(Debug)]
+pub struct Bytes<const N: usize> {
+    data: [u8; N],
+}
+
+type BS = Bytes<100>;
+
+pub trait X {
+    fn demo<const N:usize>(&self)->Bytes<N>;
+}
+

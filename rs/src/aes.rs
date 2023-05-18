8539ebaeb6f97f0d57a6c646687736a651d2d1d8
flucium <14249224+flucium@users.noreply.github.com>
2023-05-18 14:00:11 +0900

    refactor ./rs/src/aes.rs
    crate::size::*

--- rs/src/aes.rs
+++ rs/src/aes.rs
@@ -1,3 +1,4 @@
+use crate::size::*;
 use crate::Error;
 use crate::ErrorKind;
 use crate::Result;
@@ -6,7 +7,12 @@ use aes_gcm::{Aes128Gcm, Aes256Gcm};
 
 type Aes192Gcm = aes_gcm::AesGcm<aes_gcm::aes::Aes192, aead::consts::U12>;
 
-pub fn aes_128_gcm_encrypt(key: &[u8; 16], nonce: &[u8; 12], plain: &[u8]) -> Result<Vec<u8>> {
+// pub fn aes_128_gcm_encrypt(key: &[u8; 16], nonce: &[u8; 12], plain: &[u8]) -> Result<Vec<u8>> {
+pub fn aes_128_gcm_encrypt(
+    key: &[u8; SIZE_U16],
+    nonce: &[u8; SIZE_U12],
+    plain: &[u8],
+) -> Result<Vec<u8>> {
     match Aes128Gcm::new_from_slice(key) {
         Err(err) => return Err(Error::new(ErrorKind::Unknown, err.to_string())),
         Ok(ok) => match ok.encrypt(nonce.into(), plain) {
@@ -16,7 +22,12 @@ pub fn aes_128_gcm_encrypt(key: &[u8; 16], nonce: &[u8; 12], plain: &[u8]) -> Re
     }
 }
 
-pub fn aes_128_gcm_decrypt(key: &[u8; 16], nonce: &[u8; 12], cipher: &[u8]) -> Result<Vec<u8>> {
+// pub fn aes_128_gcm_decrypt(key: &[u8; 16], nonce: &[u8; 12], cipher: &[u8]) -> Result<Vec<u8>> {
+pub fn aes_128_gcm_decrypt(
+    key: &[u8; SIZE_U16],
+    nonce: &[u8; SIZE_U12],
+    cipher: &[u8],
+) -> Result<Vec<u8>> {
     match Aes128Gcm::new_from_slice(key) {
         Err(err) => return Err(Error::new(ErrorKind::Unknown, err.to_string())),
         Ok(ok) => match ok.decrypt(nonce.into(), cipher) {
@@ -26,7 +37,12 @@ pub fn aes_128_gcm_decrypt(key: &[u8; 16], nonce: &[u8; 12], cipher: &[u8]) -> R
     }
 }
 
-pub fn aes_192_gcm_encrypt(key: &[u8; 24], nonce: &[u8; 12], plain: &[u8]) -> Result<Vec<u8>> {
+// pub fn aes_192_gcm_encrypt(key: &[u8; 24], nonce: &[u8; 12], plain: &[u8]) -> Result<Vec<u8>> {
+pub fn aes_192_gcm_encrypt(
+    key: &[u8; SIZE_U24],
+    nonce: &[u8; SIZE_U12],
+    plain: &[u8],
+) -> Result<Vec<u8>> {
     match Aes192Gcm::new_from_slice(key) {
         Err(err) => return Err(Error::new(ErrorKind::Unknown, err.to_string())),
         Ok(ok) => match ok.encrypt(nonce.into(), plain) {
@@ -36,7 +52,12 @@ pub fn aes_192_gcm_encrypt(key: &[u8; 24], nonce: &[u8; 12], plain: &[u8]) -> Re
     }
 }
 
-pub fn aes_192_gcm_decrypt(key: &[u8; 24], nonce: &[u8; 12], cipher: &[u8]) -> Result<Vec<u8>> {
+// pub fn aes_192_gcm_decrypt(key: &[u8; 24], nonce: &[u8; 12], cipher: &[u8]) -> Result<Vec<u8>> {
+pub fn aes_192_gcm_decrypt(
+    key: &[u8; SIZE_U24],
+    nonce: &[u8; SIZE_U12],
+    cipher: &[u8],
+) -> Result<Vec<u8>> {
     match Aes192Gcm::new_from_slice(key) {
         Err(err) => return Err(Error::new(ErrorKind::Unknown, err.to_string())),
         Ok(ok) => match ok.decrypt(nonce.into(), cipher) {
@@ -46,7 +67,12 @@ pub fn aes_192_gcm_decrypt(key: &[u8; 24], nonce: &[u8; 12], cipher: &[u8]) -> R
     }
 }
 
-pub fn aes_256_gcm_encrypt(key: &[u8; 32], nonce: &[u8; 12], plain: &[u8]) -> Result<Vec<u8>> {
+// pub fn aes_256_gcm_encrypt(key: &[u8; 32], nonce: &[u8; 12], plain: &[u8]) -> Result<Vec<u8>> {
+pub fn aes_256_gcm_encrypt(
+    key: &[u8; SIZE_U32],
+    nonce: &[u8; SIZE_U12],
+    plain: &[u8],
+) -> Result<Vec<u8>> {
     match Aes256Gcm::new_from_slice(key) {
         Err(err) => return Err(Error::new(ErrorKind::Unknown, err.to_string())),
         Ok(ok) => match ok.encrypt(nonce.into(), plain) {
@@ -56,7 +82,12 @@ pub fn aes_256_gcm_encrypt(key: &[u8; 32], nonce: &[u8; 12], plain: &[u8]) -> Re
     }
 }
 
-pub fn aes_256_gcm_decrypt(key: &[u8; 32], nonce: &[u8; 12], cipher: &[u8]) -> Result<Vec<u8>> {
+// pub fn aes_256_gcm_decrypt(key: &[u8; 32], nonce: &[u8; 12], cipher: &[u8]) -> Result<Vec<u8>> {
+pub fn aes_256_gcm_decrypt(
+    key: &[u8; SIZE_U32],
+    nonce: &[u8; SIZE_U12],
+    cipher: &[u8],
+) -> Result<Vec<u8>> {
     match Aes256Gcm::new_from_slice(key) {
         Err(err) => return Err(Error::new(ErrorKind::Unknown, err.to_string())),
         Ok(ok) => match ok.decrypt(nonce.into(), cipher) {

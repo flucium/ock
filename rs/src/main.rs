2e1d2fca40b4493797e5f7b4b3c5271b03201595
flucium <14249224+flucium@users.noreply.github.com>
2023-05-19 12:26:15 +0900

    update ./rs/src/main.rs

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,19 +1,37 @@
+use aead::KeyInit;
+use aead::Aead;
+trait Symmetric<T, N> {
+    fn new(key: &[u8]) -> Result<T, ()>;
+    fn encrypt(&mut self, nonce: N, plain: &[u8]) -> Result<Vec<u8>, ()>;
+    fn decrypt(&mut self, nonce: N, cipher: &[u8]) -> Result<Vec<u8>, ()>;
+}
+
+struct Aes128Gcm(aes_gcm::Aes128Gcm);
+
+impl Symmetric<Aes128Gcm, &[u8; 12]> for Aes128Gcm {
+    fn new(key: &[u8]) -> Result<Self, ()> {
+        match aes_gcm::Aes128Gcm::new_from_slice(key) {
+            Err(_) => Err(()),
+            Ok(ok) => Ok(Self(ok)),
+        }
+    }
+
+    fn encrypt(&mut self, nonce: &[u8; 12], plain: &[u8]) -> Result<Vec<u8>, ()> {
+        match self.0.encrypt(nonce.into(), plain) {
+            Err(_) => Err(()),
+            Ok(cipher) => Ok(cipher),
+        }
+    }
+
+    fn decrypt(&mut self, nonce: &[u8; 12], cipher: &[u8]) -> Result<Vec<u8>, ()> {
+        match self.0.decrypt(nonce.into(), cipher) {
+            Err(_) => Err(()),
+            Ok(plain) => Ok(plain),
+        }
+    }
+}
 fn main() {
-    // let bs = Bytes { data: [9u8; 100] };
-    // print!("{:?}",bs);
-
-    Bytes{data:[0u8;100]};
-    Bytes{data:[0u8;200]};
 }
 
-#[derive(Debug)]
-pub struct Bytes<const N: usize> {
-    data: [u8; N],
-}
 
-type BS = Bytes<100>;
-
-pub trait X {
-    fn demo<const N:usize>(&self)->Bytes<N>;
-}
 

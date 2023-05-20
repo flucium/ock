f6a2b40429fc3090ed683acd4fbb8c17c46185c8
flucium <14249224+flucium@users.noreply.github.com>
2023-05-20 12:46:52 +0900

    refactor ./rs/src/lib.rs

--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -4,45 +4,52 @@ pub mod rand;
 pub mod size;
 pub mod symmetric;
 pub mod utils;
-
-pub type Result<T> = std::result::Result<T, Error>;
-
-#[derive(Debug, Clone, Default)]
-pub enum ErrorKind {
-    #[default]
-    Unknown,
-
-    Todo,
+pub(crate) use error::*;
+pub(crate) use result::Result;
+pub mod result {
+    use crate::error::Error;
+    pub type Result<T> = std::result::Result<T, Error>;
 }
 
-#[derive(Debug, Clone)]
-pub struct Error {
-    kind: ErrorKind,
-    message: String,
-}
+pub mod error {
 
-impl Error {
-    pub fn new(kind: ErrorKind, message: String) -> Self {
-        Self {
-            kind: kind,
-            message: message,
+    #[derive(Debug, Clone, Default)]
+    pub enum ErrorKind {
+        #[default]
+        Unknown,
+
+        Todo,
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct Error {
+        kind: ErrorKind,
+        message: String,
+    }
+
+    impl Error {
+        pub fn new(kind: ErrorKind, message: String) -> Self {
+            Self {
+                kind: kind,
+                message: message,
+            }
+        }
+
+        pub fn message(&self) -> &str {
+            &self.message
+        }
+
+        pub fn kind(&self) -> &ErrorKind {
+            &self.kind
         }
     }
 
-    pub fn message(&self) -> &str {
-        &self.message
-    }
-
-    pub fn kind(&self) -> &ErrorKind {
-        &self.kind
-    }
-}
-
-impl Default for Error {
-    fn default() -> Self {
-        Self {
-            kind: ErrorKind::Unknown,
-            message: Default::default(),
+    impl Default for Error {
+        fn default() -> Self {
+            Self {
+                kind: ErrorKind::Unknown,
+                message: Default::default(),
+            }
         }
     }
 }

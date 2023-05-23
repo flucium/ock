6f3f06d6e7d31bf291d019de02c2d06e1b9c3025
flucium <14249224+flucium@users.noreply.github.com>
2023-05-24 02:09:41 +0900

    update ./rs/src/deflate.rs

--- rs/src/deflate.rs
+++ rs/src/deflate.rs
@@ -4,6 +4,7 @@ use flate2::{
 };
 use std::io::{self, Read, Write};
 
+/// Deflate compress
 pub fn compress(bytes: &[u8]) -> io::Result<Vec<u8>> {
     let mut deflate = flate2::write::DeflateEncoder::new(Vec::new(), Compression::default());
     deflate.write_all(bytes)?;
@@ -12,6 +13,7 @@ pub fn compress(bytes: &[u8]) -> io::Result<Vec<u8>> {
     Ok(finish)
 }
 
+/// Deflate decompress
 pub fn decompress(bytes: &[u8]) -> io::Result<Vec<u8>> {
     let mut deflate = flate2::write::DeflateDecoder::new(Vec::new());
     deflate.write_all(bytes)?;
@@ -20,12 +22,14 @@ pub fn decompress(bytes: &[u8]) -> io::Result<Vec<u8>> {
     Ok(finish)
 }
 
+/// Deflate compress from io
 pub fn compress_from_read<R: Read>(r: R) -> io::Result<Vec<u8>> {
     let mut buf = Vec::new();
     DeflateEncoder::new(r, Compression::default()).read_to_end(&mut buf)?;
     Ok(buf)
 }
 
+/// Deflate decompress from io
 pub fn decompress_from_read<R: Read>(r: R) -> io::Result<Vec<u8>> {
     let mut buf = Vec::new();
     DeflateDecoder::new(r).read_to_end(&mut buf)?;

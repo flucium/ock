a084549d379dca66dbb744f8919dedee19c3b8d4
flucium <14249224+flucium@users.noreply.github.com>
2023-05-25 11:47:53 +0900

    clean ./rs/src/symmetric/mod.rs

--- rs/src/symmetric/mod.rs
+++ rs/src/symmetric/mod.rs
@@ -1,5 +1,4 @@
-mod chacha;
-use self::chacha::{ChaCha20Blake3, XChaCha20Blake3};
+// mod chachablake3;
 use crate::{size::*, Error, ErrorKind, Result};
 use aead::{Aead, KeyInit, Payload};
 use chacha20poly1305::ChaCha20Poly1305;
@@ -46,44 +45,44 @@ pub fn sym_decrypt(
     }
 }
 
-pub fn sym_cb3_encrypt(
-    key: &[u8; SIZE_U32],
-    nonce: &[u8; SIZE_U12],
-    aad: &[u8],
-    plain: &[u8],
-) -> Result<Vec<u8>> {
-    match ChaCha20Blake3::new_from_slice(key) {
-        Err(err) => Err(Error::new(ErrorKind::Unknown, err.to_string())),
-        Ok(ok) => match ok.encrypt(
-            nonce.into(),
-            Payload {
-                msg: plain,
-                aad: aad,
-            },
-        ) {
-            Err(err) => Err(Error::new(ErrorKind::Unknown, err.to_string())),
-            Ok(cipher) => Ok(cipher),
-        },
-    }
-}
+// pub fn sym_cb3_encrypt(
+//     key: &[u8; SIZE_U32],
+//     nonce: &[u8; SIZE_U12],
+//     aad: &[u8],
+//     plain: &[u8],
+// ) -> Result<Vec<u8>> {
+//     match ChaCha20Blake3::new_from_slice(key) {
+//         Err(err) => Err(Error::new(ErrorKind::Unknown, err.to_string())),
+//         Ok(ok) => match ok.encrypt(
+//             nonce.into(),
+//             Payload {
+//                 msg: plain,
+//                 aad: aad,
+//             },
+//         ) {
+//             Err(err) => Err(Error::new(ErrorKind::Unknown, err.to_string())),
+//             Ok(cipher) => Ok(cipher),
+//         },
+//     }
+// }
 
-pub fn sym_cb3_decrypt(
-    key: &[u8; SIZE_U32],
-    nonce: &[u8; SIZE_U12],
-    aad: &[u8],
-    cipher: &[u8],
-) -> Result<Vec<u8>> {
-    match ChaCha20Blake3::new_from_slice(key) {
-        Err(err) => Err(Error::new(ErrorKind::Unknown, err.to_string())),
-        Ok(ok) => match ok.decrypt(
-            nonce.into(),
-            Payload {
-                msg: cipher,
-                aad: aad,
-            },
-        ) {
-            Err(err) => Err(Error::new(ErrorKind::Unknown, err.to_string())),
-            Ok(plain) => Ok(plain),
-        },
-    }
-}
+// pub fn sym_cb3_decrypt(
+//     key: &[u8; SIZE_U32],
+//     nonce: &[u8; SIZE_U12],
+//     aad: &[u8],
+//     cipher: &[u8],
+// ) -> Result<Vec<u8>> {
+//     match ChaCha20Blake3::new_from_slice(key) {
+//         Err(err) => Err(Error::new(ErrorKind::Unknown, err.to_string())),
+//         Ok(ok) => match ok.decrypt(
+//             nonce.into(),
+//             Payload {
+//                 msg: cipher,
+//                 aad: aad,
+//             },
+//         ) {
+//             Err(err) => Err(Error::new(ErrorKind::Unknown, err.to_string())),
+//             Ok(plain) => Ok(plain),
+//         },
+//     }
+// }

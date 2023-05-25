74accfadf86bb2beee1d19acf551901ba870d4a7
flucium <14249224+flucium@users.noreply.github.com>
2023-05-25 11:49:10 +0900

    ./rs/src/symmetric/mod.rs
    chacha20 -> xchacha20

--- rs/src/symmetric/mod.rs
+++ rs/src/symmetric/mod.rs
@@ -1,15 +1,16 @@
 // mod chachablake3;
 use crate::{size::*, Error, ErrorKind, Result};
 use aead::{Aead, KeyInit, Payload};
-use chacha20poly1305::ChaCha20Poly1305;
+// use chacha20poly1305::ChaCha20Poly1305;
+use chacha20poly1305::XChaCha20Poly1305;
 
 pub fn sym_encrypt(
     key: &[u8; SIZE_U32],
-    nonce: &[u8; SIZE_U12],
+    nonce: &[u8; SIZE_U24],
     aad: &[u8],
     plain: &[u8],
 ) -> Result<Vec<u8>> {
-    match ChaCha20Poly1305::new_from_slice(key) {
+    match XChaCha20Poly1305::new_from_slice(key) {
         Err(err) => Err(Error::new(ErrorKind::Unknown, err.to_string())),
         Ok(ok) => match ok.encrypt(
             nonce.into(),
@@ -26,11 +27,11 @@ pub fn sym_encrypt(
 
 pub fn sym_decrypt(
     key: &[u8; SIZE_U32],
-    nonce: &[u8; SIZE_U12],
+    nonce: &[u8; SIZE_U24],
     aad: &[u8],
     cipher: &[u8],
 ) -> Result<Vec<u8>> {
-    match ChaCha20Poly1305::new_from_slice(key) {
+    match XChaCha20Poly1305::new_from_slice(key) {
         Err(err) => Err(Error::new(ErrorKind::Unknown, err.to_string())),
         Ok(ok) => match ok.decrypt(
             nonce.into(),

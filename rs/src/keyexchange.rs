ab77385b1fcfe2becdb1e8f21f58774ca5852e90
flucium <14249224+flucium@users.noreply.github.com>
2023-06-02 11:32:52 +0900

    comment out

--- rs/src/keyexchange.rs
+++ rs/src/keyexchange.rs
@@ -1,24 +1,24 @@
-use crate::rand::Rand;
-use crate::size::*;
-use x25519_dalek::{PublicKey, StaticSecret};
+// use crate::rand::Rand;
+// use crate::size::*;
+// use x25519_dalek::{PublicKey, StaticSecret};
 
 // use x25519_dalek::EphemeralSecret;
-/// Warning. i may make adjustments to this in the future.
-pub fn generate_keypair() -> ([u8; SIZE_U32], [u8; SIZE_U32]) {
+// Warning. i may make adjustments to this in the future.
+// pub fn generate_keypair() -> ([u8; SIZE_U32], [u8; SIZE_U32]) {
         
-    let private_key = StaticSecret::new(Rand);
+//     let private_key = StaticSecret::new(Rand);
 
-    let public_key = PublicKey::from(&private_key);
+//     let public_key = PublicKey::from(&private_key);
 
-    (private_key.to_bytes(), public_key.to_bytes())
-}
+//     (private_key.to_bytes(), public_key.to_bytes())
+// }
 
-/// Warning. i may make adjustments to this in the future.
-/// Currently unadjusted. not secure clone operations.
-pub fn exchange(private_key: &[u8; SIZE_U32], their_public: &[u8; SIZE_U32]) -> [u8; SIZE_U32] {
-    let private_key = StaticSecret::from(private_key.clone());
+// Warning. i may make adjustments to this in the future.
+// Currently unadjusted. not secure clone operations.
+// pub fn exchange(private_key: &[u8; SIZE_U32], their_public: &[u8; SIZE_U32]) -> [u8; SIZE_U32] {
+//     let private_key = StaticSecret::from(private_key.clone());
 
-    let shared_key = private_key.diffie_hellman(&PublicKey::from(their_public.clone()));
+//     let shared_key = private_key.diffie_hellman(&PublicKey::from(their_public.clone()));
 
-    shared_key.to_bytes()
-}
+//     shared_key.to_bytes()
+// }

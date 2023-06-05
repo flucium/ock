bdcba66f57069125cf55056bd6064e53733935ea
flucium <14249224+flucium@users.noreply.github.com>
2023-06-06 00:53:32 +0900

    clean ./rs/src/keys.rs

--- rs/src/keys.rs
+++ rs/src/keys.rs
@@ -1,8 +1,18 @@
+// use crate::{Error, ErrorKind, Result};
+// use std::fs::File;
+// use std::io;
+// use std::io::Read;
+// use std::io::Write;
+// use std::path::Path;
+// use std::collections::HashMap;
 
-pub struct Keys {}
+// #[derive(Debug)]
+// pub struct Keys {
+//     buf:  Vec<u8>,
+// }
 
-impl Keys {
-    pub fn new() -> Self {
-        todo!()
-    }
-}
+// impl Keys {
+//     pub fn new() -> Self {
+//         Self { buf: Vec::new() }
+//     }
+// }

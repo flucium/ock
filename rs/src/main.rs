ffb8c83f1f4611d32da9482bf01da7094f0d9e11
flucium <14249224+flucium@users.noreply.github.com>
2023-06-07 02:13:58 +0900

    clean ./rs/src/main.rs

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,5 +1,9 @@
-use std::io::{self, Read, Write};
-
+use ock::{self, size::*};
+use std::{
+    self,
+    io::{stderr, Write},
+    process::exit,
+};
 const NAME: &str = "OCK";
 
 const VERSION: &str = "0.0.1";
@@ -14,11 +18,41 @@ fn app() -> clap::Command {
         .author(AUTHOR)
         .about(ABOUT)
         .subcommands([
-            clap::Command::new("aes-128-gcm").args(app_symmetric_args()),
-            clap::Command::new("aes-192-gcm").args(app_symmetric_args()),
-            clap::Command::new("aes-256-gcm").args(app_symmetric_args()),
-            clap::Command::new("chacha20-poly1305").args(app_symmetric_args()),
+            clap::Command::new("aes-128-gcm")
+                .args(app_symmetric_args())
+                .args(app_format_args())
+                .args(app_compress_args()),
+            clap::Command::new("aes-192-gcm")
+                .args(app_symmetric_args())
+                .args(app_format_args())
+                .args(app_compress_args()),
+            clap::Command::new("aes-256-gcm")
+                .args(app_symmetric_args())
+                .args(app_format_args())
+                .args(app_compress_args()),
+            clap::Command::new("chacha20-poly1305")
+                .args(app_symmetric_args())
+                .args(app_format_args())
+                .args(app_compress_args()),
         ])
+}
+
+fn app_compress_args() -> [clap::Arg; 1] {
+    [clap::Arg::new("deflate")
+        .long("deflate")
+        .action(clap::ArgAction::SetTrue)]
+}
+
+fn app_format_args() -> [clap::Arg; 2] {
+    [
+        clap::Arg::new("base64")
+            .long("base64")
+            .alias("b64")
+            .action(clap::ArgAction::SetTrue),
+        clap::Arg::new("hex")
+            .long("hex")
+            .action(clap::ArgAction::SetTrue),
+    ]
 }
 
 fn app_symmetric_args() -> [clap::Arg; 4] {
@@ -47,32 +81,114 @@ fn app_symmetric_args() -> [clap::Arg; 4] {
     ]
 }
 
-fn stdout(bytes: &[u8]) {
-    if let Err(err) = io::stdout().write_all(bytes) {
-        panic!("{err}")
-    }
-}
-
-fn stderr(bytes: &[u8]) {
-    if let Err(err) = io::stderr().write_all(bytes) {
-        panic!("{err}")
-    }
-}
-
-/*
-
-*/
-
-fn main() {
+fn main() -> std::io::Result<()> {
     let app = app();
 
     let matches = app.get_matches();
 
     match matches.subcommand() {
-        Some(("aes-128-gcm", arg_matches)) => {}
-        Some(("aes-192-gcm", arg_matches)) => {}
-        Some(("aes-256-gcm", arg_matches)) => {}
-        Some(("chacha20-poly1305", arg_matches)) => {}
+        Some(("aes-128-gcm", arg_matches)) => {
+            let _key: &[u8; SIZE_U16] = match arg_matches
+                .get_one::<String>("key")
+                .unwrap_or(&String::default())
+                .as_bytes()
+                .try_into()
+            {
+                Err(err) => {
+                    write!(&mut stderr(), "{:?}", err)?;
+
+                    exit(1)
+                }
+                Ok(key) => key,
+            };
+
+            let _message = arg_matches
+                .get_one::<String>("message")
+                .unwrap_or(&String::default())
+                .as_bytes();
+
+            if arg_matches.get_flag("encrypt") {}
+            if arg_matches.get_flag("decrypt") {
+            } else {
+            }
+        }
+        Some(("aes-192-gcm", arg_matches)) => {
+            let _key: &[u8; SIZE_U24] = match arg_matches
+                .get_one::<String>("key")
+                .unwrap_or(&String::default())
+                .as_bytes()
+                .try_into()
+            {
+                Err(err) => {
+                    write!(&mut stderr(), "{:?}", err)?;
+
+                    exit(1)
+                }
+                Ok(key) => key,
+            };
+
+            let _message = arg_matches
+                .get_one::<String>("message")
+                .unwrap_or(&String::default())
+                .as_bytes();
+
+            if arg_matches.get_flag("encrypt") {}
+            if arg_matches.get_flag("decrypt") {
+            } else {
+            }
+        }
+        Some(("aes-256-gcm", arg_matches)) => {
+            let _key: &[u8; SIZE_U32] = match arg_matches
+                .get_one::<String>("key")
+                .unwrap_or(&String::default())
+                .as_bytes()
+                .try_into()
+            {
+                Err(err) => {
+                    write!(&mut stderr(), "{:?}", err)?;
+
+                    exit(1)
+                }
+                Ok(key) => key,
+            };
+
+            let _message = arg_matches
+                .get_one::<String>("message")
+                .unwrap_or(&String::default())
+                .as_bytes();
+
+            if arg_matches.get_flag("encrypt") {}
+            if arg_matches.get_flag("decrypt") {
+            } else {
+            }
+        }
+        Some(("chacha20-poly1305", arg_matches)) => {
+            let _key: &[u8; SIZE_U32] = match arg_matches
+                .get_one::<String>("key")
+                .unwrap_or(&String::default())
+                .as_bytes()
+                .try_into()
+            {
+                Err(err) => {
+                    write!(&mut stderr(), "{:?}", err)?;
+
+                    exit(1)
+                }
+                Ok(key) => key,
+            };
+
+            let _message = arg_matches
+                .get_one::<String>("message")
+                .unwrap_or(&String::default())
+                .as_bytes();
+
+            if arg_matches.get_flag("encrypt") {}
+            if arg_matches.get_flag("decrypt") {
+            } else {
+            }
+        }
         _ => {}
     }
+
+    Ok(())
 }

87bf6e8af04a6fe12ccc500cf1069c8508e0ba6d
flucium <14249224+flucium@users.noreply.github.com>
2023-06-07 00:43:33 +0900

    clean ./rs/src/main.rs

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -69,95 +69,10 @@ fn main() {
     let matches = app.get_matches();
 
     match matches.subcommand() {
-        Some(("aes-128-gcm", arg_matches)) => {
-            let key = arg_matches
-                .get_one("key")
-                .unwrap_or(&String::default())
-                .to_owned();
-
-            let message = arg_matches
-                .get_one("message")
-                .unwrap_or(&String::default())
-                .to_owned();
-
-            if arg_matches.get_flag("encrypt") {
-                let c = ock::symmetric::Symmetric::Aes128Gcm {
-                    key: Box::new(key.as_bytes().try_into().unwrap()),
-                    aad: b"",
-                    data: message.as_bytes(),
-                }
-                .encrypt()
-                .unwrap();
-
-                println!("{:?}", c);
-            } else if arg_matches.get_flag("decrypt") {
-            } else {
-            }
-        }
+        Some(("aes-128-gcm", arg_matches)) => {}
         Some(("aes-192-gcm", arg_matches)) => {}
         Some(("aes-256-gcm", arg_matches)) => {}
         Some(("chacha20-poly1305", arg_matches)) => {}
         _ => {}
     }
 }
-
-/*
-    Encrypt : encrypt enc e
-    Decrypt : decrypt dec d
-    Output  : output out o
-    Input   : input in i
-    File    : file f
-    Length  : length len l
-    Key     : key k
-    Password: password psw p
-    Generate: generate gen g
-
-
-    --AES--
-    aes-128-gcm
-    aes-192-gcm
-    aes-256-gcm
-
-    --ChaCha--
-    chacha20-poly1305
-
-    --Random--
-    random
-
-*/
-
-// clap::Arg::new("password")
-//     .long("password")
-//     .short('p')
-//     .alias("psw");
-
-// clap::Arg::new("generate")
-//     .long("generate")
-//     .short('g')
-//     .alias("gen");
-
-// clap::Arg::new("key").long("key").short('k');
-
-// clap::Arg::new("length")
-//     .long("length")
-//     .short('l')
-//     .alias("len");
-
-// clap::Arg::new("file").long("file").short('f');
-
-// clap::Arg::new("input").long("input").short('i').alias("in");
-
-// clap::Arg::new("output")
-//     .long("output")
-//     .short('o')
-//     .alias("out");
-
-// clap::Arg::new("decrypt")
-//     .long("decrypt")
-//     .short('d')
-//     .alias("dec");
-
-// clap::Arg::new("encrypt")
-//     .long("encrypt")
-//     .short('e')
-//     .alias("enc");
